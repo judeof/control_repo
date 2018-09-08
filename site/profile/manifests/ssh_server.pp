6b5713fd5ca467adc03a38bd0e9949536f0d10f8
judeof <37797723+judeof@users.noreply.github.com>
2018-09-08 19:36:34 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDsbc/Z7EpqFNviXWaMObkXAe03xR5llIllGvlZKh+ghHJmAElEEkia/krMNe73MN+1uCT9gQeNm/rFbxPTsyKJ6yRPiOvK6NQ+esdoSvXfKOgc8ryy+TDXbIuNs/kvW2JVzTWiJ+ZWuJPGxcDSK8V7iv8bazle0bc/WYylHOzr2P6u7cyIKv6ZeO0nb+PniMQpaCPrH95WlBkigs1RO9NLzYb+PNdfcgUxM8bu9w+8bH12XvIZn7HyotH4EVuSjVLFaDI3SDD8JUp+Bn/AiQ1aPJrtX7WCzHohUORBfdDEoTwQrp2lxmyafAP8d7RJ1R+QQkfCLtfwAKT38Ke3O/VV',
 	}  
 }
